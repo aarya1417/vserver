6b47fe77467e2a4370bebf3bc5cbe35d59cec712
aarya1417 <80144198+aarya1417@users.noreply.github.com>
2025-06-08 20:19:12 +0530

    Update server.py

--- server.py
+++ server.py
@@ -24,4 +24,4 @@ def get_coords():
         return jsonify({"lat": 0, "lon": 0, "timestamp": None})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
